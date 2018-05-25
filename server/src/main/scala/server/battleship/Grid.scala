f500c0380bc7c419586f13bb911eb17d64484c8c
Vladimir Mikovski Iotov <vladimir.mikovski.iotov@ing.nl>
2018-05-25 16:17:43 +0200

    Add requirements for shipPlacement

--- server/src/main/scala/server/battleship/Grid.scala
+++ server/src/main/scala/server/battleship/Grid.scala
@@ -1,5 +1,7 @@
 package server.battleship
 
+import server.battleship.Grid.ShipPlacement
+
 sealed trait Message
 
 case object YouBeatMe extends Message
@@ -18,7 +20,24 @@ object Grid {
 
   }
 
+  val size = 10
+
   case class ShipPlacement(ship: Ship, row: Int, col: Char, isHorizontal: Boolean, hits: Set[(Int, Char)]) {
+    require({
+      val leftCorrect = col >= 'A'
+      val rightCorrect =
+        if (isHorizontal) col + ship.size <= 'A' + size
+        else col <= 'A' + size
+      leftCorrect && rightCorrect
+    }, "Column out of boundaries")
+    require({
+      val upCorrect = row >= 1
+      val downCorrect =
+        if (isHorizontal) row <= size
+        else row + ship.size <= 1 + size
+      upCorrect && downCorrect
+    }, "Row out of boundaries")
+
 
     def isSunk = ???
   }
@@ -28,14 +47,10 @@ object Grid {
 case class Grid(shipPlacements: Set[Grid.ShipPlacement]) {
   require(shipPlacements.size == 5, "Shipment number is incorect: " + shipPlacements.size + " instead of 5")
   require(shipPlacements.map(_.ship.name) == Ship.allShipTypes, "Not all types in ship placements")
-  require(shipPlacements.filter(s => s.col))
 
 
   def attack(row: Int, col: Char): (Grid, Option[Message]) = {
     ???
   }
-
-
-
 
 }
--- server/src/test/scala/server/battleship/GridSpec.scala
+++ server/src/test/scala/server/battleship/GridSpec.scala
@@ -2,6 +2,7 @@ package server.battleship
 
 import org.scalatest.{Matchers, WordSpec}
 import Grid.ShipPlacement.Direction._
+import server.battleship.Grid.ShipPlacement
 
 class GridSpec extends WordSpec with Matchers {
 
@@ -66,26 +67,20 @@ class GridSpec extends WordSpec with Matchers {
 
     }
 
-    /*  A B C D E F G H I J
-      1 # # # # # * * * * *
-      2 * * * * * * * # # #
-      3 * * * * * * * * * *
-      4 * * * # * * * * * *
-      5 * * * # * * * * * *
-      6 * * * # * * * * * *
-      7 * * * # * * * * * *
-      8 * * * * * * * * * *
-      9 # * * * * * * * * *
-     10 # * * * * * * * # # #
-  */
-    "return false, if ship is out of boundaries" in {
 
-      val outOfBoundaries = correctShipPlacements.map(s => if (s.ship.name == "submarine") s.copy(col = 'I') else s)
-      val thrown = intercept[Exception] {
-        Grid(outOfBoundaries)
-      }
-      assert(thrown.getMessage === "Boom!")
-
+    /*    A B C D E F G H I J
+        1 # # # # * * * * * *
+        2 * * * * * * * * * *
+        3 * * * * * * * * * *
+        4 * * * * * * * * * *
+        5 * * * * * * * * * *
+        6 * * * * * * * * * *
+        7 * * * * * * * * * *
+        8 * * * * * * * * * *
+        9 * * * * * * * * * *
+       10 * * * * * * * * * *
+    */
+    "Fails to create Grid if one ship is on top of the other" in {
 
     }
 
@@ -101,26 +96,87 @@ class GridSpec extends WordSpec with Matchers {
         9 * * * * * * * * * *
        10 * * * * * * * * * *
     */
-    "return false if one ship is on top of the other" in {
-
-    }
-
-    /*    A B C D E F G H I J
-        1 # # # # * * * * * *
-        2 * * * * * * * * * *
-        3 * * * * * * * * * *
-        4 * * * * * * * * * *
-        5 * * * * * * * * * *
-        6 * * * * * * * * * *
-        7 * * * * * * * * * *
-        8 * * * * * * * * * *
-        9 * * * * * * * * * *
-       10 * * * * * * * * * *
-    */
-    "return false if group of ships is not correct" in {
+    "Fails to create Grid if group of ships is not correct" in {
 
     }
   }
 
+  "ShipPlacement" should {
+    /*    A B C D E F G H I J
+        1 # # # # * * * * * *
+        2 * * * * * * * * * *
+        3 * * * * * * * * * *
+        4 * * * * * * * * * *
+        5 * * * * * * * * * *
+        6 * * * * * * * * * *
+        7 * * * * * * * * * *
+        8 * * * * * * * * * *
+        9 * * * * * * * * * *
+       10 * * * * * * * * * *
+    */
+    "creates object, if the shipPlacement is inside the Grid" in {
+      val _ = Grid.ShipPlacement(Ship.carrier, 1, 'A', HORIZONTAL, Set())
+    }
+
+    "fail to create ShipPlacement, if the shipPlacement column is outside the Grid" in {
+      val thrown = intercept[IllegalArgumentException] {
+        val wrongPlacement = carrier.copy(col = ('A' + Grid.size + 1).toChar)
+      }
+      assert(thrown.getMessage === "requirement failed: Column out of boundaries")
+
+    }
+
+    "fail to create ShipPlacement, if the shipPlacement column is a special character" in {
+      val thrown = intercept[IllegalArgumentException] {
+        val wrongPlacement = carrier.copy(col = 13.toChar)
+      }
+      assert(thrown.getMessage === "requirement failed: Column out of boundaries")
+
+    }
+
+    "fail to create ShipPlacement, if the shipPlacement row is outside the Grid" in {
+      val thrown = intercept[IllegalArgumentException] {
+        val wrongPlacement = carrier.copy(row = Grid.size + 1)
+      }
+      assert(thrown.getMessage === "requirement failed: Row out of boundaries")
+    }
+
+    "fail to create ShipPlacement, if the shipPlacement row is a negative value" in {
+      val thrown = intercept[IllegalArgumentException] {
+        val wrongPlacement = carrier.copy(row = -1)
+      }
+      assert(thrown.getMessage === "requirement failed: Row out of boundaries")
+    }
+
+    "fail to create ShipPlacement, if the shipPlacement vertical with ship out of boundaries" in {
+      val thrown = intercept[IllegalArgumentException] {
+        val wrongPlacement = carrier.copy(row = 10, isHorizontal = false)
+      }
+      assert(thrown.getMessage === "requirement failed: Row out of boundaries")
+
+    }
+
+    /*    A B C D E F G H I J
+        1 * * * * * * * * * *
+        2 * * * * * * * * * *
+        3 * * * * * * * * * *
+        4 * * * * * * * * * *
+        5 * * * * * * * * * *
+        6 * * * * * * * * * *
+        7 * * * * * * * * * *
+        8 # * * * * * * * * *
+        9 # * * * * * * * * *
+       10 # * * * * * * * * *
+          #
+    */
+    "fail to create ShipPlacement, if the shipPlacement vertical with ship out of boundaries TEST" in {
+      val thrown = intercept[IllegalArgumentException] {
+        val wrongPlacement = carrier.copy(row = 8, isHorizontal = false)
+      }
+      assert(thrown.getMessage === "requirement failed: Row out of boundaries")
+
+    }
+
+  }
 
 }
