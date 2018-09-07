fe0a12c697d02b5fc941f260b8d7f86223b5b41c
Jordi de Vos <jordi.de.vos@ing.nl>
2018-09-07 16:00:43 +0200

    add test for playeractorspec

--- server/src/test/scala/server/actors/PlayerActorSpec.scala
+++ server/src/test/scala/server/actors/PlayerActorSpec.scala
@@ -1,15 +1,29 @@
 package server.actors
 
 import akka.actor.ActorSystem
-import akka.testkit.TestKit
+import akka.testkit.{TestKit, TestProbe}
 import org.scalatest.WordSpecLike
-import server.actors.PlayerActorBehavior
+import server.actors.Protocol.Attack
+import server.battleship.GridImpl.ShipPlacement
+import server.battleship.Player
 
 class PlayerActorSpec extends TestKit(ActorSystem("PlayerActor")) with WordSpecLike {
 
   "PlayerActor" should {
     "respond with an attack when receiving GetAttack" in new PlayerActorBehavior {
+      val attackLocation = (2, 'C')
 
+      val playerLogic = new Player {
+        val shipPlacements: Set[ShipPlacement] = Set.empty
+
+        def getAttack: (Int, Char) = attackLocation
+      }
+
+      val probe = TestProbe("Testprobe")
+
+      handleGetAttack(playerLogic, probe.ref)
+
+      probe.expectMsg(Attack(attackLocation._1, attackLocation._2))
     }
   }
 
